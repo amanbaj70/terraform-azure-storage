d981f64c73a82fd6b329aa63947a8193f17c0d67
Aman <77617424+amanbaj70@users.noreply.github.com>
2023-12-12 12:08:07 +0000

    Add files via upload

---
+++ modules/storageaccount/variables.tf
@@ -0,0 +1,23 @@
+variable "resourcegroupname" {
+  description = "The resource group name"
+}
+
+variable "resourcegplocation" {
+  description = "Location of RG"
+  default = "East US"
+}
+
+variable "storageaccountname" {
+  description = "This is the name of storage accoutn to be created"
+  default = "vishnustorageaccount"
+}
+
+variable "tier" {
+  description = "Trier of the storage account"
+  default =  "Hot"
+}
+
+variable "replication" {
+  description = "This is the type of Replication"
+  default = "LRS"
+}
